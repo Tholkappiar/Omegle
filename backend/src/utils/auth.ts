aebc965a0867e7435e1494ccb68b196731cfb87b
tholkappiarwk <tholkappiar@abcdeventures.com>
2025-05-22 16:14:03 +0530

    backend: feat - verify email

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "../../generated/prisma/client";
-import { openAPI } from "better-auth/plugins";
+import { multiSession, openAPI } from "better-auth/plugins";
 import { CONFIG } from "./config";
 
 const prisma = new PrismaClient();
@@ -9,10 +9,27 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-    emailAndPassword: { enabled: true },
     trustedOrigins: [CONFIG.FRONTEND_URL],
-    plugins: [openAPI()],
+    plugins: [
+        openAPI(),
+        multiSession({
+            maximumSessions: 0,
+        }),
+    ],
+    emailAndPassword: { enabled: true, requireEmailVerification: true },
+    emailVerification: {
+        sendOnSignUp: true,
+        autoSignInAfterVerification: true,
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            console.log(url);
+            // await sendEmail({
+            //     to: user.email,
+            //     subject: 'Verify your email address',
+            //     text: `Click the link to verify your email: ${url}`
+            // })
+        },
+    },
     session: {
-        expiresIn: 30, // 30 sec for test
+        expiresIn: 60 * 2, // 120 sec for test
     },
 });
--- backend/src/websocket/websocket.ts
+++ backend/src/websocket/websocket.ts
@@ -3,6 +3,6 @@ import apiRoutes from "../controllers/apiRoutes";
 import websocketRoutes from "../controllers/clientRoutes/clientHandler";
 
 export function registerRoutes(app: Express): void {
-    app.use("/api", apiRoutes);
+    app.use("/", apiRoutes);
     app.use("/api/websocket", websocketRoutes);
 }
